f61f775ba1816e7b6adc511eaca3b179e8e399ea
listentodella <27765011+listentodella@users.noreply.github.com>
2024-07-10 13:56:26 +0800

    Update my settings.lua

--- lua/user_template/settings.lua
+++ lua/user_template/settings.lua
@@ -6,4 +6,25 @@ settings["use_ssh"] = true
 
 settings["colorscheme"] = "catppuccin"
 
+settings["lsp_deps"] = function(defaults)
+        return {
+                "bashls",
+                "clangd",
+                "html",
+                "jsonls",
+                "lua_ls",
+                "pylsp",
+        }
+end
+
+settings["null_ls_deps"] = function(defaults)
+        return {
+                "clang_format",
+                "prettier",
+                "shfmt",
+                "stylua",
+                "vint",
+        }
+end
+
 return settings
